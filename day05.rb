502c78a04df7c5c08a7aeb55e99361863228cba2
Johnny Dongelmans <johnny.dongelmans@beequip.nl>
2019-12-05 16:29:26 +0100

    whew! Day05::Part2 🤯

--- day05.rb
+++ day05.rb
@@ -6,27 +6,29 @@ class Day05
     Part2.new.run
   end
 
-  def self.calculate(input, current_position)
-    case input[(current_position += 1)]
+  def self.calculate(input, current_position = 0)
+    instruction = input[current_position].to_s.rjust(5, '0')
+    opcode      = instruction[3..-1].to_i
+
+    case opcode
     when 3, 4
       number_of_parameters = 1
+    when 99
+      number_of_parameters = 0
     else
       number_of_parameters = 3
     end
 
-    group = input[current_position..(current_position += number_of_parameters)]
+    return input if opcode == 99
 
-    instruction = group[0].to_s.rjust(5, '0')
-    opcode      = instruction[3..-1].to_i
+    group = input[current_position..(current_position += number_of_parameters)]
 
     param_1_mode = (instruction[2] || 0).to_i
     value_1      = param_1_mode == 0 ? input[group[1]] : group[1]
 
-    unless [3, 4].include?(opcode)
+    if number_of_parameters > 1
       param_2_mode = (instruction[1] || 0).to_i
       value_2      = param_2_mode == 0 ? input[group[2]] : group[2]
-      param_3_mode = (instruction[0] || 0).to_i
-      value_3      = param_3_mode == 0 ? input[group[3]] : group[3]
     end
 
     case opcode
@@ -38,24 +40,34 @@ class Day05
       puts "Input: "
       input[group[1]] = gets.chomp.to_i
     when 4
-      puts "Output for opcode 4: #{input[group[1]]}"
-    when 99
-      return input
+      puts "Output for opcode 4: #{value_1}"
+    when 5
+      current_position = value_2 if value_1 != 0
+    when 6
+      current_position = value_2 if value_1 == 0
+    when 7
+      input[group[3]] = (value_1 < value_2) ? 1 : 0
+    when 8
+      input[group[3]] = (value_1 == value_2) ? 1 : 0
+    else
+      raise "Invalid opcode: #{opcode}"
     end
 
+    current_position += 1 unless [5, 6].include?(opcode)
     calculate(input, current_position)
   end
 
   class Part1
     def run
       input = File.read('./day05.txt').split(',').map(&:to_i)
-
-      Day05.calculate(input, -1)
+      Day05.calculate(input)
     end
   end
 
   class Part2
     def run
+      input = File.read('./day05.txt').split(',').map(&:to_i)
+      Day05.calculate(input)
     end
   end
 end
